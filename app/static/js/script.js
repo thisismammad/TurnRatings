859363e414276b8e64451cb9b24fcef6cc5f8ef0
Mammad <mamaddavari07@gmail.com>
2021-04-30 22:12:42 +0430

    add live search fixed change position in edit fixed clear modal after colse modal 1400/2/10

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -477,3 +477,36 @@ $('#modal-add-item').on('hidden.bs.modal', function (e) {
          .prop("checked", "")
          .end();
   })
+
+  $(document).ready(function() {
+    $('#data-tabel').DataTable( {
+        language: {
+            "sEmptyTable": "هیچ داده‌ای در جدول وجود ندارد",
+            "sInfo": "نمایش _START_ تا _END_ از _TOTAL_ ردیف",
+            "sInfoEmpty": "نمایش 0 تا 0 از 0 ردیف",
+            "sInfoFiltered": "(فیلتر شده از _MAX_ ردیف)",
+            "sInfoThousands": ",",
+            "sLengthMenu": "نمایش _MENU_ ردیف",
+            "sLoadingRecords": "در حال بارگزاری...",
+            "sProcessing": "در حال پردازش...",
+            "sSearch": "",
+            "searchPlaceholder": "جستجو...",
+            "sZeroRecords": "رکوردی با این مشخصات پیدا نشد",
+            "oPaginate": {
+              "sFirst": "برگه‌ی نخست",
+              "sLast": "برگه‌ی آخر",
+              "sNext": "بعدی",
+              "sPrevious": "قبلی"
+            },
+            "oAria": {
+              "sSortAscending": ": فعال سازی نمایش به صورت صعودی",
+              "sSortDescending": ": فعال سازی نمایش به صورت نزولی"
+            }
+          },
+          "paging":   false,
+          "order": [],
+          "lengthChange": false,
+          "info": false, 
+    } );
+} );
+
